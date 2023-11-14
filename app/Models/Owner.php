d27924610a8aa73e00050ec56ebed1d3c788ea74
irfannm21 <irfannurmuhammad21@gmail.com>
2023-11-14 15:49:39 +0700

    FillamentAdminPanel

---
+++ app/Models/Owner.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Patient;
+
+class Owner extends Model
+{
+    use HasFactory;
+
+    // public function patients()
+    // {
+    //     return $this->hasMany(Patient::class);
+    // }
+}
---
+++ app/Models/Treatment.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Casts\MoneyCast;
+
+class Treatment extends Model
+{
+    use HasFactory;
+
+    protected $casts = [
+        'price' => MoneyCast::class,
+    ];
+}
